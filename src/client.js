179b6dd30b12b6c0a34dcb88d6942c44f9c53dd9
frankpagan <frank@cocreate.app>
2023-08-04 16:10:54 -0600

    feat(client.js): Add generateDB and create functions
    - Added new functions named generateDB and create to the client.js file.
    - generateDB function generates a new database using the Crud library and creates organization, user, key, and role objects in the indexeddb storage.
    - create function handles form submission, gets the data from the form, and creates an organization using the Crud library and the createOrganization method.
    - These changes enable the creation of organizations and the generation of a new database with associated objects.

--- src/client.js
+++ src/client.js
@@ -1,61 +1,152 @@
-import crud from '@cocreate/crud-client';
-import indexeddb from '@cocreate/indexeddb';
-import action from '@cocreate/actions';
-import form from '@cocreate/form';
+import Crud from '@cocreate/crud-client';
+import Action from '@cocreate/actions';
+import Form from '@cocreate/form';
 
-const CoCreateOrganization = {
 
-    createOrganization: async function (btn) {
-        let formEl = btn.closest("form");
-        if (!formEl) return;
+async function generateDB(organization = { object: {} }, user = { object: {} }) {
+    const organization_id = organization.object._id || Crud.ObjectId();
+    const defaultKey = organization.object.key || uuid.generate();
+    const user_id = user.object._id || Crud.ObjectId();
 
-        let organization = form.getData(formEl, 'organizations')
-        let user = form.getData(formEl, 'users')
+    let apiKey = await Crud.send({ method: 'create.object', database: organization_id, array: 'keys', organization_id })
+    if (apiKey && apiKey.object && apiKey.object[0])
+        return
 
-        if (!organization || !organization.object)
-            return
-        if (!user || !user.object)
-            return
+    try {
+        // Create organization 
+        organization.method = 'create.object'
+        organization.storage = 'indexeddb'
+        organization.database = organization_id
+        organization.array = 'organizations'
+        organization.object._id = organization_id
+        organization.object.name = organization.object.name || 'untitiled'
+        organization.organization_id = organization_id
+        Crud.send(organization);
 
-        if (!organization.object._id && !user.object._id) {
-            let objects = await indexeddb.generateDB(organization, user)
-            if (!objects)
-                return
-        }
+        // Create user
+        user.method = 'create.object'
+        user.storage = 'indexeddb'
+        user.database = organization_id
+        user.array = 'users'
+        user.object._id = user_id
+        user.object.firstname = user.object.firstname || 'untitiled'
+        user.object.lastname = user.object.lastname || 'untitiled'
+        user.organization_id = organization_id
+        Crud.send(user);
 
-        form.setObjectId(formEl, organization)
-        form.setObjectId(formEl, user)
-
-        organization = organization.object[0]
-        user = user.object[0]
-
-        let organization_id = organization._id
-
-        if (crud.socket.organization !== true) {
-            crud.socket.organization = true
-            crud.socket.create({ organization_id })
-        }
-
-        let response = await crud.socket.send({
-            method: 'createOrganization',
-            organization,
-            user,
-            broadcastBrowser: false,
+        // Create default key
+        let key = {
+            method: 'create.object',
+            storage: 'indexeddb',
+            database: organization_id,
+            array: 'keys',
+            object: {
+                _id: Crud.ObjectId(),
+                type: "key",
+                key: defaultKey,
+                actions: {
+                    signIn: true,
+                    signUp: true
+                },
+                default: true
+            },
             organization_id
-        });
+        }
+        Crud.send(key);
 
-        document.dispatchEvent(new CustomEvent('createdOrganization', {
-            detail: response
-        }));
+        // Create role
+        let role_id = Crud.ObjectId();
+        let role = {
+            method: 'create.object',
+            storage: 'indexeddb',
+            database: organization_id,
+            array: 'keys',
+            object: {
+                _id: role_id,
+                type: "role",
+                name: "admin",
+                admin: "true"
+            },
+            organization_id
+        };
+        Crud.send(role);
+
+        // Create user key
+        let userKey = {
+            method: 'create.object',
+            storage: 'indexeddb',
+            database: organization_id,
+            array: 'keys',
+            object: {
+                _id: Crud.ObjectId(),
+                type: "user",
+                key: user_id,
+                array: 'users', // could be any array
+                roles: [role_id],
+                email: user.object.email,
+                password: user.object.password || btoa('0000')
+            },
+            organization_id
+        };
+        Crud.send(userKey);
+
+        return { organization: organization.object, key: key.object, user: user.object, role: role.object, userKey: userKey.object }
+
+    } catch (error) {
+        return false
     }
-};
+}
 
-action.init({
+async function create(btn) {
+    let formEl = btn.closest("form");
+    if (!formEl) return;
+
+    let organization = Form.getData(formEl, 'organizations')
+    let user = Form.getData(formEl, 'users')
+
+    if (!organization || !organization.object)
+        return
+    if (!user || !user.object)
+        return
+
+    if (!organization.object._id && !user.object._id) {
+        let objects = await generateDB(organization, user)
+        if (!objects)
+            return
+    }
+
+    Form.setObjectId(formEl, organization)
+    Form.setObjectId(formEl, user)
+
+    organization = organization.object[0]
+    user = user.object[0]
+
+    let organization_id = organization._id
+
+    if (Crud.socket.organization !== true) {
+        Crud.socket.organization = true
+        Crud.socket.create({ organization_id })
+    }
+
+    let response = await Crud.socket.send({
+        method: 'createOrganization',
+        organization,
+        user,
+        broadcastBrowser: false,
+        organization_id
+    });
+
+    document.dispatchEvent(new CustomEvent('createdOrganization', {
+        detail: response
+    }));
+}
+
+Action.init({
     name: "createOrganization",
     endEvent: "createdOrganization",
-    callback: (data) => {
-        CoCreateOrganization.createOrganization(data.element);
+    callback: (action) => {
+        CoCreateOrganization.create(action.element);
     }
 });
 
-export default CoCreateOrganization;
+export default { generateDB, create };
